25a0717c3ec7dc8d4cff369f22be361995de2e38
ahmedhassan2k <ah2k.dev@gmail.com>
2023-02-10 23:12:21 +0500

    Initial commit

---
+++ Class 1/script.js
@@ -0,0 +1,41 @@
+// Javascript ES6, Promises, array methods, DOM overview (practice array methods must)
+function test() {
+    let arr = [1,1,2,3,2]
+    let arr2 = [4,5,6]
+    // let output = arr.forEach((val, ind)=>{
+    //     console.log(val, ind)
+    //     return [arr[ind], arr[ind+1]]
+    // })
+    // for(let i = 0; i < arr.length; i++){
+    //     // console.log(arr[i])
+    //     if(arr[i+1] == undefined){
+    //         output.push([arr[i]])
+    //     } else {
+    //         output.push([arr[i], arr[i+1]])
+    //     }
+    //     //  output.push([arr[i], arr[i+1]])
+    // }
+    // [[1,2], [2,3], [3,4], [4,5], [5]]
+    // let output = arr.filter((val) => val != 1)
+    // let output = arr.find((val) => val == 5)
+    // let output = arr.findIndex((val) => val == 3)
+    // arr[output] = 9
+    // let output = arr2.concat(arr)
+    // console.log(output)
+}
+
+test()
+
+// variables
+// primitive, non primitive
+// primitive: string, number, boolean, null, undefined, symbol
+// non primitive: object, array, function
+// functions
+// conditionals (if, else, else if), switch case, ternary operator
+// loops (for, while, do while)
+
+
+//array methods
+// map, filter, find, findIndex, forEach, concat, join, split, includes, indexOf, push, pop, toString
+
+//
